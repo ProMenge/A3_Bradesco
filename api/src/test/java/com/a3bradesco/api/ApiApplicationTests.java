6949032e008d04cbf7784557db410a0e3274f6e9
ProMenge <mengeprofissional@gmail.com>
2025-04-30 17:59:49 -0300

    :sparkles: feat: Setup API spring project

---
+++ api/src/test/java/com/a3bradesco/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a3bradesco.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
