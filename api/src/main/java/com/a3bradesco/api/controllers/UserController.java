003e3479ed9cc5a0e3f67122c816f03ffa507932
Igor Molina <igoramil452@gmai.com>
2025-05-14 19:15:20 -0300

    :sparkles: feat: added post method for user

--- api/src/main/java/com/a3bradesco/api/controllers/UserController.java
+++ api/src/main/java/com/a3bradesco/api/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.a3bradesco.api.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.a3bradesco.api.entities.User;
 import com.a3bradesco.api.services.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/users")
@@ -30,4 +35,16 @@ public class UserController {
         User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
     }
+
+    @PostMapping()
+    public ResponseEntity<User> post(@RequestBody User userObj) {
+        userObj = userService.insert(userObj);
+
+        //Devolve no header o location (url) onde pode-se encontrar o usuário criado através de get
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				  .buildAndExpand(userObj.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(userObj);
+    }
+    
 }
--- api/src/main/java/com/a3bradesco/api/services/UserService.java
+++ api/src/main/java/com/a3bradesco/api/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
         Optional<User> userObject = userRepository.findById(id);
         return userObject.get();
     }
+
+    public User insert(User user){
+        return userRepository.save(user);
+    }
 }
