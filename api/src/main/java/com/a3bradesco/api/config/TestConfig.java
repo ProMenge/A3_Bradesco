cd2c9bc91aa26cd63bb8698d82c39163633ba649
Igor Molina <igoramil452@gmai.com>
2025-05-24 12:39:33 -0300

    feat: changed initial db seeding

--- api/src/main/java/com/a3bradesco/api/config/TestConfig.java
+++ api/src/main/java/com/a3bradesco/api/config/TestConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.a3bradesco.api.entities.User;
+import com.a3bradesco.api.entities.enums.ReportType;
 import com.a3bradesco.api.repositories.ReportRepository;
 import com.a3bradesco.api.repositories.UserRepository;
 
@@ -29,12 +30,14 @@ public class TestConfig implements CommandLineRunner {
         User user2 = new User(null, "Fred", "22222222222",  "11988888888", "fred@gmail.com", "123");
         User user3 = new User(null, "Caue", "33333333333",  "11977777777", "caue@gmail.com", "123");
 
-        Report report1 = new Report(null, user1, "cpnj", "cpf", "phoneN", "site.com");
-        Report report2 = new Report(null, user1, "cpnj2", "cpf2", "phoneN2", "site.com2");
-        Report report3 = new Report(null, user2, "cpnj2", "cpf2", "phoneN2", "site.com2");
+        Report report1 = new Report(null, user1, ReportType.CPF, "12312312312");
+        Report report2 = new Report(null, user1, ReportType.CNPJ, "92.221.230000107");
+        Report report3 = new Report(null, user2, ReportType.EMAIL, "mockemail@gmail.com");
+        Report report4 = new Report(null, user2, ReportType.SITE, "mocksite.com.br");
+        Report report5 = new Report(null, user3, ReportType.CELLPHONE, "11988887777");
 
 		userRepository.saveAll(Arrays.asList(user1, user2, user3));
         
-        reportRepository.saveAll(Arrays.asList(report1, report2, report3));
+        reportRepository.saveAll(Arrays.asList(report1, report2, report3, report4, report5));
 	}
 }
