48fbbce381ce13066ff751bef7d03bc344c74a20
IgorMolinaLS <igoramil452@gmail.com>
2025-05-24 21:40:06 -0300

    refactor: ensure importing only necessary imports through project

--- api/src/main/java/com/a3bradesco/api/repositories/UserRepository.java
+++ api/src/main/java/com/a3bradesco/api/repositories/UserRepository.java
@@ -1,10 +1,9 @@
 package com.a3bradesco.api.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.a3bradesco.api.entities.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
