c22c114b200577e4ab840e980e1988a4739ce53a
CaueUrbini <caueurbini@gmail.com>
2025-05-31 13:30:51 -0300

    ✨ feat: update code and add new files

---
+++ api/src/test/java/com/a3bradesco/api/controllers/CellphoneReportControllerTest.java
@@ -0,0 +1,113 @@
+package com.a3bradesco.api.controllers;
+
+import com.a3bradesco.api.entities.CellphoneReport;
+import com.a3bradesco.api.services.CellphoneReportService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@AutoConfigureMockMvc(addFilters = false) // Ignorei a segurança do teste para evitar o erro 403, desativando a autenticação e autorização 
+@WebMvcTest(CellphoneReportController.class) 
+class CellphoneReportControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CellphoneReportService service;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private final String validCellphone = "11992078740";
+
+    @BeforeEach
+    void setup() {
+        CellphoneReport report = new CellphoneReport(validCellphone, 1, LocalDate.now());
+        when(service.saveNewReport(anyString())).thenReturn(report);
+    }
+
+    // 1. Cellphone com 11 dígitos(com o DDD incluso) - deve retornar Created (201)
+    @Test
+    void whenValidCellphone_thenReturnsCreated() throws Exception {
+        String json = "{ \"cellphone\": \"" + validCellphone + "\" }";
+
+        mockMvc.perform(post("/cellphone-reports")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isCreated());
+    }
+
+    // 2. Número com menos de 11 ou mais dígitos - retorna BadRequest
+    @Test
+    void whenCWithInvalidLength_thenReturnsBadRequest() throws Exception {
+        String json = "{ \"cellphone\": \"123\" }";
+
+        mockMvc.perform(post("/cellphone-reports")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isBadRequest());
+    }
+
+    // 3. Campo Cellphone vazio - retorna BadRequest
+    @Test
+    void whenCellphoneBlank_thenReturnsBadRequest() throws Exception {
+        String json = "{ \"cellphone\": \"\" }";
+
+        mockMvc.perform(post("/cellphone-reports")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isBadRequest());
+    }
+
+    // 4 - Nome do campo diferente de 'cellphone' - retorna BadRequest
+    @Test
+    void whenWrongFieldName_thenReturnsBadRequest() throws Exception {
+        String json = "{ \"documento\": \"" + validCellphone + "\" }";
+
+        mockMvc.perform(post("/cellphone-reports")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isBadRequest());
+    }
+    // 5 - Cellphone com caracteres alfabéticos deve ser inválido - retorna BadRequest 
+    @Test
+    void whenCellphoneContainsLetters_thenReturnsBadRequest() throws Exception {
+    String invalidCellphone = "11T920B87A0"; // inválido
+
+        mockMvc.perform(post("/cellphone-reports")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\"cellphone\": \"" + invalidCellphone + "\"}"))
+            .andExpect(status().isBadRequest());
+}
+    // 6 - Cellphone com símbolos como ".", "-"  - retorna BadRequest
+    @Test
+    void whenCellphoneHasSymbols_thenReturnsBadRequest() throws Exception {
+    String cellphoneWithSymbols = "11.992'078-740";
+
+    mockMvc.perform(post("/cellphone-reports")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\"cellphone\": \"" + cellphoneWithSymbols + "\"}"))
+            .andExpect(status().isBadRequest());
+}
+
+
+
+
+
+}
